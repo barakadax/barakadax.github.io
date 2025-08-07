3e042d83ae910398092a03e38814f77711a3ce97
barakadax <barakadax@gmail.com>
2025-08-07 17:43:46 +0300

    added support for none touchscreen menu sliding

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,9 @@ menuController.menuScreen = document.getElementById('menuScreen');
 menuController.touchstartX = 0;
 menuController.touchendX = 0;
 menuController.minSwipeDistance = 40;
+menuController.mouseDown = false;
+menuController.mouseStartX = 0;
+menuController.mouseEndX = 0;
 
 Object.defineProperty(menuController, 'handleSwipe', {
     writable: false,
@@ -44,6 +47,32 @@ menuController.menu.addEventListener('touchend', (event) => {
   menuController.handleSwipe();
 });
 
+menuController.menu.addEventListener('mousedown', (event) => {
+  if (event.button !== 0) return;
+  menuController.mouseDown = true;
+  menuController.mouseStartX = event.screenX;
+  menuController.mouseEndX = event.screenX;
+
+  const onMouseMove = (moveEvent) => {
+    if (!menuController.mouseDown) return;
+    menuController.mouseEndX = moveEvent.screenX;
+  };
+
+  const onMouseUp = (upEvent) => {
+    if (!menuController.mouseDown) return;
+    menuController.mouseDown = false;
+    menuController.mouseEndX = upEvent.screenX;
+    menuController.touchstartX = menuController.mouseStartX;
+    menuController.touchendX = menuController.mouseEndX;
+    menuController.handleSwipe();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
 menuController.menu.addEventListener('click', () => {
   menuController.menu.classList.toggle('open');
   const isOpen = menuController.menu.classList.contains('open');
@@ -56,7 +85,7 @@ Object.defineProperty(menuController, 'adjustMenuPosition', {
     value: function() {
       const bodyWidth = document.body.scrollWidth;
 
-      if (bodyWidth < 1024) {
+      if (bodyWidth <= 1070) {
         menuController.menu.style.top = '10px';
         menuController.menu.style.right = '10px';
       } else if (bodyWidth > 2000) {
